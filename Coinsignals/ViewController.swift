1523733605345371596c40913afcb7fe6ab37dc0
JULIAN! <julian.praest@julians-uniq-016-mac.local>
2018-12-21 17:11:09 +0100

    Initial Commit

---
+++ Coinsignals/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coinsignals
+//
+//  Created by Julian Praest on 21.12.18.
+//  Copyright © 2018 Org. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
